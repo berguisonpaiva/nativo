4e5fcf75af42fc9811d061d4b0c4b511048e48f5
berguisonpaiva <pberguison@gmail.com>
2021-07-11 14:21:03 -0300

    firebase

--- android/app/build/generated/source/buildConfig/debug/br/com/capivaratecnologia/nativo/BuildConfig.java
+++ android/app/build/generated/source/buildConfig/debug/br/com/capivaratecnologia/nativo/BuildConfig.java
@@ -5,7 +5,7 @@ package br.com.capivaratecnologia.nativo;
 
 public final class BuildConfig {
   public static final boolean DEBUG = Boolean.parseBoolean("true");
-  public static final String APPLICATION_ID = "br.com.capivaratecnologias.nativo";
+  public static final String APPLICATION_ID = "br.com.capivaratecnologia.nativo";
   public static final String BUILD_TYPE = "debug";
   public static final int VERSION_CODE = 1;
   public static final String VERSION_NAME = "1.0.0";
